b63e9f5f6d034a1655f3852660c510a2a9aa95b9
MOF <101332404+MoiconK@users.noreply.github.com>
2025-04-30 09:14:49 +0200

    Cambio de prueba

--- src/Pruebas_del_Proyecto/Ventana_Consultas_Clientes.java
+++ src/Pruebas_del_Proyecto/Ventana_Consultas_Clientes.java
@@ -66,7 +66,7 @@ public class Ventana_Consultas_Clientes extends JFrame {
 			public void actionPerformed(ActionEvent e) 
 			{
 				Ventana_Buscar_Libros B1 = new Ventana_Buscar_Libros();
-				B1.setVisible(true);
+				B1.setVisible(false);
 			}
 		});
 		btn_BuscarLibros.setFont(new Font("Tahoma", Font.PLAIN, 30));
