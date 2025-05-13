e8dd36af8aae7d2363d84072732b8921c549bfcb
AdanGavira <gaviradan@gmail.com>
2025-05-13 14:41:48 +0200

    ventana principal

--- src/Prueba_Pryecto/Ventana_Principal.java
+++ src/Prueba_Pryecto/Ventana_Principal.java
@@ -9,6 +9,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -55,14 +57,30 @@ public class Ventana_Principal extends JFrame {
 		setContentPane(contentPane);
 		
 		setLocationRelativeTo(null);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{552, 552, 0};
-		gbl_contentPane.rowHeights = new int[]{287, 287, 287, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lbl_titulo = new JLabel("LA BIBLIOTECA DEL GÉNESIS");
+		lbl_titulo.setBounds(5, 5, 547, 282);
+		lbl_titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_titulo.setBackground(new Color(245, 227, 200));
+		lbl_titulo.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		contentPane.add(lbl_titulo);
+		
+		final JLabel lblDelGenesis = new JLabel("");
+		lblDelGenesis.setBounds(557, 5, 552, 282);
+		lblDelGenesis.setBackground(new Color(245, 227, 200));
+		lblDelGenesis.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDelGenesis.setIcon(new ImageIcon(Ventana_Principal.class.getResource("/image/Logotipo_Biblioteca_del_Génesis 256x256.png")));
+		contentPane.add(lblDelGenesis);
 		
 		JButton btn_Registro_Usuarios = new JButton("Registro de clientes");
+		btn_Registro_Usuarios.setBounds(557, 292, 552, 284);
+		btn_Registro_Usuarios.setBorderPainted(false);
+		btn_Registro_Usuarios.setContentAreaFilled(false);
+		ImageIcon icono3 = new ImageIcon (Ventana_Principal.class.getResource("/image/registro_clientes_escala.png"));
+		Image imagen3 = icono3.getImage().getScaledInstance(btn_Registro_Usuarios.getWidth(), btn_Registro_Usuarios.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon iconoAjustado3 = new ImageIcon (imagen3);
+		btn_Registro_Usuarios.setIcon(iconoAjustado3);
 		btn_Registro_Usuarios.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
@@ -70,39 +88,17 @@ public class Ventana_Principal extends JFrame {
 				V1.setVisible(true);
 			}
 		});
+		contentPane.add(btn_Registro_Usuarios);
 		
-		JLabel lbl_titulo = new JLabel("LA BIBLIOTECA DEL GÉNESIS");
-		lbl_titulo.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_titulo.setBackground(new Color(245, 227, 200));
-		lbl_titulo.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		GridBagConstraints gbc_lbl_titulo = new GridBagConstraints();
-		gbc_lbl_titulo.fill = GridBagConstraints.BOTH;
-		gbc_lbl_titulo.insets = new Insets(0, 0, 5, 5);
-		gbc_lbl_titulo.gridx = 0;
-		gbc_lbl_titulo.gridy = 0;
-		contentPane.add(lbl_titulo, gbc_lbl_titulo);
-		
-		final JLabel lblDelGenesis = new JLabel("");
-		lblDelGenesis.setBackground(new Color(245, 227, 200));
-		lblDelGenesis.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDelGenesis.setIcon(new ImageIcon(Ventana_Principal.class.getResource("/image/Logotipo_Biblioteca_del_Génesis 256x256.png")));
-		lblDelGenesis.setBounds(0, 0, 1720, 900);
-		GridBagConstraints gbc_lblDelGenesis = new GridBagConstraints();
-		gbc_lblDelGenesis.fill = GridBagConstraints.BOTH;
-		gbc_lblDelGenesis.insets = new Insets(0, 0, 5, 0);
-		gbc_lblDelGenesis.gridx = 1;
-		gbc_lblDelGenesis.gridy = 0;
-		contentPane.add(lblDelGenesis, gbc_lblDelGenesis);
 		
 		JButton btn_Consulta_Libros = new JButton(" Consultas de libros");
-		btn_Consulta_Libros.setBackground(new Color(255, 128, 128));
-		btn_Consulta_Libros.setFont(new Font("Tahoma", Font.BOLD, 30));
-		GridBagConstraints gbc_btn_Consulta_Libros = new GridBagConstraints();
-		gbc_btn_Consulta_Libros.fill = GridBagConstraints.BOTH;
-		gbc_btn_Consulta_Libros.insets = new Insets(0, 0, 5, 5);
-		gbc_btn_Consulta_Libros.gridx = 0;
-		gbc_btn_Consulta_Libros.gridy = 1;
-		contentPane.add(btn_Consulta_Libros, gbc_btn_Consulta_Libros);
+		btn_Consulta_Libros.setBounds(5, 587, 547, 284);
+		btn_Consulta_Libros.setBorderPainted(false);
+		btn_Consulta_Libros.setContentAreaFilled(false);
+		ImageIcon icono4 = new ImageIcon (Ventana_Principal.class.getResource("/image/consulta_libros_escala.png"));
+		Image imagen4 = icono4.getImage().getScaledInstance(btn_Consulta_Libros.getWidth(), btn_Consulta_Libros.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon iconoAjustado4 = new ImageIcon (imagen4);
+		btn_Consulta_Libros.setIcon(iconoAjustado4);
 		btn_Consulta_Libros.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
@@ -110,39 +106,63 @@ public class Ventana_Principal extends JFrame {
 				C1.setVisible(true);
 			}
 		});
-		btn_Registro_Usuarios.setBackground(new Color(255, 128, 128));
-		btn_Registro_Usuarios.setFont(new Font("Tahoma", Font.BOLD, 30));
-		GridBagConstraints gbc_btn_Registro_Usuarios = new GridBagConstraints();
-		gbc_btn_Registro_Usuarios.fill = GridBagConstraints.BOTH;
-		gbc_btn_Registro_Usuarios.insets = new Insets(0, 0, 5, 0);
-		gbc_btn_Registro_Usuarios.gridx = 1;
-		gbc_btn_Registro_Usuarios.gridy = 1;
-		contentPane.add(btn_Registro_Usuarios, gbc_btn_Registro_Usuarios);
+		contentPane.add(btn_Consulta_Libros);
+		
 		
 		JButton btn_Prestamos_Libros = new JButton("Préstamo de libros");
-		btn_Prestamos_Libros.setBackground(new Color(255, 128, 128));
-		btn_Prestamos_Libros.setFont(new Font("Tahoma", Font.BOLD, 30));
-		GridBagConstraints gbc_btn_Prestamos_Libros = new GridBagConstraints();
-		gbc_btn_Prestamos_Libros.fill = GridBagConstraints.BOTH;
-		gbc_btn_Prestamos_Libros.insets = new Insets(0, 0, 0, 5);
-		gbc_btn_Prestamos_Libros.gridx = 0;
-		gbc_btn_Prestamos_Libros.gridy = 2;
-		contentPane.add(btn_Prestamos_Libros, gbc_btn_Prestamos_Libros);
+		btn_Prestamos_Libros.setBounds(5, 292, 547, 284);
+		btn_Prestamos_Libros.setBorderPainted(false);
+		btn_Prestamos_Libros.setContentAreaFilled(false);
+		ImageIcon icono2 = new ImageIcon(Ventana_Principal.class.getResource("/image/prestamo_libros_escala.png"));
+		Image imagen2 = icono2.getImage().getScaledInstance(btn_Prestamos_Libros.getWidth(), btn_Prestamos_Libros.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon iconoAjustado2 = new ImageIcon (imagen2);
+		btn_Prestamos_Libros.setIcon(iconoAjustado2);
+		btn_Prestamos_Libros.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				Ventana_Prestamos_Libros P1 = new Ventana_Prestamos_Libros();
+				P1.setVisible(true);
+			}
+		});
+		contentPane.add(btn_Prestamos_Libros);
+	
 		
-		JButton btn_Consultas_Usuario = new JButton("Consultas de clientes");
+		JButton btn_Consultas_Usuario = new JButton("Registro de Clientes");
+		btn_Consultas_Usuario.setBorderPainted(false);
+		btn_Consultas_Usuario.setContentAreaFilled(false);
+		btn_Consultas_Usuario.setBounds(5, 292, 547, 284);
+		ImageIcon icono5 = new ImageIcon(Ventana_Principal.class.getResource("/image/registro_clientes_escala"));
+		Image imagen5 = icono2.getImage().getScaledInstance(btn_Consultas_Usuario.getWidth(), btn_Consultas_Usuario.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon iconoAjustado5 = new ImageIcon (imagen3);
+		btn_Consultas_Usuario.setIcon(iconoAjustado3);
 		btn_Consultas_Usuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				Ventana_Consultas_Clientes CC1 = new Ventana_Consultas_Clientes();
-				CC1.setVisible(true);
+				Ventana_Consultas_Clientes C6 = new Ventana_Consultas_Clientes();
+				C6.setVisible(true);
 			}
 		});
-		btn_Consultas_Usuario.setBackground(new Color(255, 128, 128));
-		btn_Consultas_Usuario.setFont(new Font("Tahoma", Font.BOLD, 30));
-		GridBagConstraints gbc_btn_Consultas_Usuario = new GridBagConstraints();
-		gbc_btn_Consultas_Usuario.fill = GridBagConstraints.BOTH;
-		gbc_btn_Consultas_Usuario.gridx = 1;
-		gbc_btn_Consultas_Usuario.gridy = 2;
-		contentPane.add(btn_Consultas_Usuario, gbc_btn_Consultas_Usuario);
+		contentPane.add(btn_Prestamos_Libros);
+		
+		
+		JButton btn_Consultas_Clientes = new JButton(" Consultas Clientes");
+		btn_Consultas_Clientes.setBorderPainted(false);
+		btn_Consultas_Clientes.setContentAreaFilled(false);
+		btn_Consultas_Clientes.setBounds(5, 292, 547, 284);
+		ImageIcon icono6 = new ImageIcon(Ventana_Principal.class.getResource("/image/registro_clientes_escala"));
+		Image imagen6 = icono6.getImage().getScaledInstance(btn_Consultas_Clientes.getWidth(), btn_Consultas_Clientes.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon iconoAjustado6 = new ImageIcon (imagen6);
+		btn_Consultas_Usuario.setIcon(iconoAjustado6);
+		contentPane.add(btn_Consultas_Clientes);
+		
+		JButton btnNewButton = new JButton("Consulta Clientes");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}
+		});
+		btnNewButton.setBounds(557, 587, 549, 284);
+		contentPane.add(btnNewButton);
 	}
 }
